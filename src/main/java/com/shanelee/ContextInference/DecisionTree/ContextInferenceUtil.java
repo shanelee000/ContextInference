7f2829232c50c12190c785cf927918705ab54f78
litong <asd476093193@163.com>
2016-05-13 01:16:30 +0800

    update decision tree

--- src/main/java/com/shanelee/ContextInference/DecisionTree/ContextInferenceUtil.java
+++ src/main/java/com/shanelee/ContextInference/DecisionTree/ContextInferenceUtil.java
@@ -1,10 +1,12 @@
 package com.shanelee.ContextInference.DecisionTree;
 
 import com.shanelee.ContextInference.entity.AttributeEntity;
+import com.shanelee.ContextInference.entity.TreeEntity;
 import com.sun.xml.internal.messaging.saaj.packaging.mime.util.LineInputStream;
 import org.hibernate.validator.constraints.URL;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -56,19 +58,19 @@ public class ContextInferenceUtil {
         int sizeOfList = attrList.size();
 
         if ("light".equals(featureName)) {
-
+            newListMap = buildLightListMap(attrList);
         } else if("sound".equals(featureName)) {
-
+            newListMap = buildSoundListMap(attrList);
         } else if("temperature".equals(featureName)) {
-
+            newListMap = buildTemperatureListMap(attrList);
         } else if("humidity".equals(featureName)) {
-
+            newListMap = buildHumidityListMap(attrList);
         } else if("position".equals(featureName)) {
-
+            newListMap = buildPositionListMap(attrList);
         } else if("movement".equals(featureName)) {
-
+            newListMap = buildMovementListMap(attrList);
         } else if("gps".equals(featureName)) {
-
+            newListMap = buildGpsListMap(attrList);
         }
 
         double conditionEntropyOfDataSet = 0;
@@ -79,7 +81,188 @@ public class ContextInferenceUtil {
         return conditionEntropyOfDataSet;
     }
 
-    private static Map<String, List<AttributeEntity>> buildLightListMap(List<AttributeEntity> attrList){
+    /**
+     * 构造决策树
+     * @return
+     */
+    private static TreeEntity buildDT(){
         return null;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildLightListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfVeryBright = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfBright = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfNormal = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfDark = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfVeryDark = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("very bright".equals(attr.getLight())){
+                attrOfVeryBright.add(attr);
+            } else if ("bright".equals(attr.getLight())){
+                attrOfBright.add(attr);
+            } else if ("normal".equals(attr.getLight())){
+                attrOfNormal.add(attr);
+            } else if ("dark".equals(attr.getLight())){
+                attrOfDark.add(attr);
+            } else if ("very dark".equals(attr.getLight())){
+                attrOfVeryDark.add(attr);
+            }
+        }
+
+        newListMap.put("very bright", attrOfVeryBright);
+        newListMap.put("bright", attrOfBright);
+        newListMap.put("normal", attrOfNormal);
+        newListMap.put("dark", attrOfDark);
+        newListMap.put("very dark", attrOfVeryDark);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildSoundListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfVeryLoud = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfLoud = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfNormal = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfQuiet = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfVeryQuiet = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("very loud".equals(attr.getSound())){
+                attrOfVeryLoud.add(attr);
+            } else if ("loud".equals(attr.getSound())){
+                attrOfLoud.add(attr);
+            } else if ("normal".equals(attr.getSound())){
+                attrOfNormal.add(attr);
+            } else if ("quiet".equals(attr.getSound())){
+                attrOfQuiet.add(attr);
+            } else if ("very quiet".equals(attr.getSound())){
+                attrOfVeryQuiet.add(attr);
+            }
+        }
+
+        newListMap.put("very loud", attrOfVeryLoud);
+        newListMap.put("loud", attrOfLoud);
+        newListMap.put("normal", attrOfNormal);
+        newListMap.put("quiet", attrOfQuiet);
+        newListMap.put("very quiet", attrOfVeryQuiet);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildTemperatureListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfVeryHot = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfHot = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfNormal = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfCold = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfVeryCold = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("very hot".equals(attr.getTemperature())){
+                attrOfVeryHot.add(attr);
+            } else if ("hot".equals(attr.getTemperature())){
+                attrOfHot.add(attr);
+            } else if ("normal".equals(attr.getTemperature())){
+                attrOfNormal.add(attr);
+            } else if ("cold".equals(attr.getTemperature())){
+                attrOfCold.add(attr);
+            } else if ("very cold".equals(attr.getTemperature())){
+                attrOfVeryCold.add(attr);
+            }
+        }
+
+        newListMap.put("very hot", attrOfVeryHot);
+        newListMap.put("hot", attrOfHot);
+        newListMap.put("normal", attrOfNormal);
+        newListMap.put("cold", attrOfCold);
+        newListMap.put("very cold", attrOfVeryCold);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildHumidityListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfHigh = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfMedium = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfLow = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("high".equals(attr.getHumidity())){
+                attrOfHigh.add(attr);
+            } else if ("medium".equals(attr.getHumidity())){
+                attrOfMedium.add(attr);
+            } else if ("low".equals(attr.getHumidity())){
+                attrOfLow.add(attr);
+            }
+        }
+
+        newListMap.put("high", attrOfHigh);
+        newListMap.put("medium", attrOfMedium);
+        newListMap.put("low", attrOfLow);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildPositionListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfLying = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfStanding = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("lying".equals(attr.getPosition())){
+                attrOfLying.add(attr);
+            } else if ("standing".equals(attr.getPosition())){
+                attrOfStanding.add(attr);
+            }
+        }
+
+        newListMap.put("lying", attrOfLying);
+        newListMap.put("standing", attrOfStanding);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildMovementListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfNotMoving = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfMoving = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfMovingFast = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("not moving".equals(attr.getMovement())){
+                attrOfNotMoving.add(attr);
+            } else if ("moving".equals(attr.getMovement())){
+                attrOfMoving.add(attr);
+            } else if ("moving fast".equals(attr.getMovement())){
+                attrOfMovingFast.add(attr);
+            }
+        }
+
+        newListMap.put("not moving", attrOfNotMoving);
+        newListMap.put("moving", attrOfMoving);
+        newListMap.put("moving fast", attrOfMovingFast);
+        return newListMap;
+    }
+
+    private static Map<String, List<AttributeEntity>> buildGpsListMap(List<AttributeEntity> attrList){
+
+        Map<String, List<AttributeEntity>> newListMap = new HashMap<String, List<AttributeEntity>>();
+        List<AttributeEntity> attrOfIndoor = new ArrayList<AttributeEntity>();
+        List<AttributeEntity> attrOfOutdoor = new ArrayList<AttributeEntity>();
+
+        for (AttributeEntity attr : attrList) {
+            if("indoor".equals(attr.getGps())){
+                attrOfIndoor.add(attr);
+            } else if ("outdoor".equals(attr.getGps())){
+                attrOfOutdoor.add(attr);
+            }
+        }
+
+        newListMap.put("indoor", attrOfIndoor);
+        newListMap.put("outdoor", attrOfOutdoor);
+        return newListMap;
     }
 }
---
+++ src/main/java/com/shanelee/ContextInference/entity/TreeEntity.java
@@ -0,0 +1,31 @@
+package com.shanelee.ContextInference.entity;
+
+import java.util.List;
+
+/**
+ * 决策树模型
+ */
+public class TreeEntity {
+
+    //该节点上方的引导分类属性
+    private String attribute;
+
+    //该节点下方的子节点
+    private List<TreeEntity> children;
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public List<TreeEntity> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<TreeEntity> children) {
+        this.children = children;
+    }
+}
